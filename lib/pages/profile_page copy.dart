e47423c5c55bb205d2e330b783c146cabdaf3889
nattavee tantragool <devilzeros@outlook.co.th>
2020-12-27 23:30:15 +0700

    est

--- lib/pages/profile_page copy.dart
+++ lib/pages/profile_page copy.dart
@@ -1,31 +1,47 @@
-import 'package:common_bottom_navigation_bar/screens/screen2.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.lightBlueAccent,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-//        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Container(
-            child: Text(
-              'Screen 1',
-              style: TextStyle(color: Colors.white, fontSize: 20),
+    return Material(
+      child: Center(
+        child: Shortcuts(
+          shortcuts: <LogicalKeySet, Intent>{
+            // Pressing space in the field will now move to the next field.
+            LogicalKeySet(LogicalKeyboardKey.space): const NextFocusIntent(),
+          },
+          child: FocusTraversalGroup(
+            child: Form(
+              autovalidateMode: AutovalidateMode.always,
+              onChanged: () {
+                Form.of(primaryFocus.context).save();
+              },
+              child: Wrap(
+                children: List<Widget>.generate(5, (int index) {
+                  return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          icon: Icon(Icons.person),
+                          hintText: 'What do people call you?',
+                          labelText: 'Name *',
+                        ),
+                        onSaved: (String value) {
+                          // This optional block of code can be used to run
+                          // code when the user saves the form.
+                        },
+                        validator: (String value) {
+                          return value.contains('@')
+                              ? 'Do not use the @ char.'
+                              : null;
+                        },
+                      ));
+                }),
+              ),
             ),
-            margin: EdgeInsets.all(16),
           ),
-          FlatButton(
-            onPressed: () {
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => Screen2()));
-            },
-            child: Text('Go to next screen'),
-            color: Colors.white,
-          ),
-        ],
+        ),
       ),
     );
   }
--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,31 +1,132 @@
-import 'package:common_bottom_navigation_bar/screens/screen2.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart' show DragStartBehavior;
+import 'dart:async' show Future;
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:flutter/cupertino.dart';
 
 class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.lightBlueAccent,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-//        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Container(
-            child: Text(
-              'Screen 1',
-              style: TextStyle(color: Colors.white, fontSize: 20),
+    return Scaffold(
+      body: Container(
+        child: MyStatefulWidget(),
+      ),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  MyStatefulWidget({Key key}) : super(key: key);
+
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+/// This is the private State class that goes with MyStatefulWidget.
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  String dropdownValue = 'เลือกทีม';
+  final _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    const sizedBoxSpace = SizedBox(height: 24);
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        alignment: Alignment.center,
+        child: Form(
+          key: _formKey,
+          // autovalidateMode: _autoValidateMode,
+          child: Scrollbar(
+            child: SingleChildScrollView(
+              dragStartBehavior: DragStartBehavior.down,
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  sizedBoxSpace,
+                  Image(image: AssetImage('assets/image/test_view.jpg')),
+                  sizedBoxSpace,
+                  CupertinoButton.filled(
+                    child: Text('Upload'),
+                    onPressed: () {},
+                  ),
+                  sizedBoxSpace,
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      filled: true,
+                      icon: Icon(Icons.person),
+                      labelText: 'Username *',
+                      hintText: 'Username 8 - 12',
+                    ),
+                  ),
+                  sizedBoxSpace,
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      filled: true,
+                      icon: Icon(Icons.person),
+                      labelText: 'Password *',
+                      hintText: 'Password 8 - 12 , a-z',
+                    ),
+                    obscureText: true,
+                  ),
+                  sizedBoxSpace,
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      filled: true,
+                      icon: Icon(Icons.person),
+                      labelText: 'Confilm Password *',
+                      hintText: 'Password',
+                    ),
+                    obscureText: true,
+                  ),
+                  sizedBoxSpace,
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      filled: true,
+                      icon: Icon(Icons.person),
+                      labelText: 'NickName *',
+                      hintText: 'What Your Name ?',
+                    ),
+                  ),
+                  sizedBoxSpace,
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      filled: true,
+                      icon: Icon(Icons.person),
+                      labelText: 'LineName *',
+                      hintText: 'Line Name ?',
+                    ),
+                  ),
+                  sizedBoxSpace,
+                  DropdownButton<String>(
+                    value: dropdownValue,
+                    icon: Icon(Icons.arrow_downward),
+                    iconSize: 24,
+                    elevation: 16,
+                    style: TextStyle(color: Colors.deepPurple),
+                    // underline: Container(
+                    //   height: 2,
+                    //   color: Colors.deepPurpleAccent,
+                    // ),
+                    onChanged: (String newValue) {
+                      setState(() {
+                        dropdownValue = newValue;
+                      });
+                    },
+                    items: <String>['เลือกทีม', 'Two', 'Free', 'Four']
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                  sizedBoxSpace,
+                ],
+              ),
             ),
-            margin: EdgeInsets.all(16),
           ),
-          FlatButton(
-            onPressed: () {
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => Screen2()));
-            },
-            child: Text('Go to next screen'),
-            color: Colors.white,
-          ),
-        ],
+        ),
       ),
     );
   }
